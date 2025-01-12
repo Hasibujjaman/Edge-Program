534cb1f6fc304c55a70734b9047b3f184ec2380d
Hasibujjaman <2003001@student.ruet.ac.bd>
2025-01-12 19:34:02 +0600

    added something more

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -1,9 +1,13 @@
-<ul>
-    @if (count($users) > 0)
-        @foreach ($users as $user)
-            <li>{{ $user->name }}</li>
-        @endforeach
-    @else
-        <p>No users found.</p>
-    @endif
-</ul>
+@extends('layouts.app')
+@section('title', 'User List')
+@section('content')
+    <ul>
+        @if (count($users) > 0)
+            @foreach ($users as $user)
+                <li>{{ $user->name }}</li>
+            @endforeach
+        @else
+            <p>No users found.</p>
+        @endif
+    </ul>
+@endsection
